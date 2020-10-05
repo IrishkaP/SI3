06accf36e32c4653855d4f8e70cf70d4fc5da53f
popcornhub <nikolay173@yandex.ru>
2020-10-05 15:53:51 +0500

    05.10

---
+++ глава 8 номер 14/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int max(const vector <int>& result) {
+	int max = result[result.size() - 1], ch = result.size() - 1;
+	for (int i = 0; i < result.size(); i++)
+		if (result[i] > max) {
+			max = result[i];
+			ch = i;
+		}
+	return ch;
+}
+
+int min(const vector <int>& result) {
+	int min = result[result.size() - 1],ch = result.size() - 1;
+	for (int i = 0; i < result.size(); i++)
+		if (result[i] < min) {
+			min = result[i];
+			ch = i;
+		}
+	return ch;
+}
+
+vector <int> kol(const vector <string>& start) {
+	vector <int> result;
+	for (int i = 0; i < start.size(); i++) {
+		result.push_back(start[i].size());
+	}
+	return result;
+}
+
+vector <string> sort(vector <string> start) {
+	vector <string> sorti;
+	for (int i = 0; i < start.size()-1; i++)
+		for (int j = i+1; j < start.size(); j++)
+			if (start[j] < start[i]) {
+				swap(start[j], start[i]);
+			}
+	sorti.push_back(start[0]);
+	sorti.push_back(start[start.size() - 1]);
+	return sorti;
+}
+
+
+int main()
+{
+	vector <string> start = {"Stroka_1", "Vtoraya stroka", "Chto-to", "Kkkk","AAAAAAAA"};
+	vector <int> result = kol(start);
+	for (int i = 0; i < result.size(); i++)
+		cout << result[i] << ' ';
+	cout << endl << start[min(result)] << endl << start[max(result)];
+	vector <string> sorti = sort(start);
+	cout << endl << sorti[0] << endl << sorti[1];
+}
+
+
---
+++ глава 8 номер 5/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+vector <int> first(vector <int> one) {
+	vector <int> local;
+	for (int i = one.size() - 1; i >= 0; i--)
+		local.push_back(one[i]);
+	return local;
+}
+
+void second(vector <int> &one) {
+	for (int i = 0; i < one.size() / 2; ++i)
+	{
+		swap(one[i], one[one.size()-1 - i]);
+	}
+}
+
+int main()
+{
+	vector <int> one = { 1,3,5,9 };
+	vector <int> two = first(one);
+	second(one);
+	for (int i = 0; i < two.size(); i++)
+		cout << two[i]<< ' ';
+	cout << endl;
+	for (int i = 0; i < one.size(); i++)
+		cout << one[i] << ' ';
+
+}
+
+
