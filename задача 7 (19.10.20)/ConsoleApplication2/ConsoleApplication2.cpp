cd64eaa0f27dd2a39d8d1400743fa2a8572f3486
IrishkaP <ira.pechenkina.00@mail.ru>
2020-11-02 14:55:08 +0500

    Новая задачка

---
+++ задача 7 (19.10.20)/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include "Queue.h"
+#include "Bad_area.h"
+using namespace std;
+
+
+
+int main()
+{
+	try {
+		setlocale(LC_ALL, "Russian");
+		int m, n, e;
+		char operation;
+		cin >> m;
+		Queue ex;
+		for (int i = 0; i < m; i++) {
+			cin >> operation;
+			if (operation == '+') {
+				cin >> n;
+				ex.push(n);
+				cout << "Ok" << endl;
+			}
+			if (operation == '-') {
+				e = ex.pop();
+				cout << e << endl;
+			}
+		}
+	}
+	catch (Bad_area) {
+		cout << "Ошибочка :(";
+	}
+}
+
