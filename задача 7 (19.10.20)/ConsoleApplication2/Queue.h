e8ec98712ad450e22bd7a8ebf855591004a1eff7
IrishkaP <ira.pechenkina.00@mail.ru>
2020-11-09 15:02:58 +0500

    Задача 7 с деструктором

--- задача 7 (19.10.20)/ConsoleApplication2/Queue.h
+++ задача 7 (19.10.20)/ConsoleApplication2/Queue.h
@@ -8,6 +8,30 @@ class Queue
 
 public:
 	Queue() : length(0), currentLength(0), elements(0) {}
+	Queue(const Queue& item) {
+		elements = new int[item.length];
+		length = item.length;
+		currentLength = item.currentLength;
+		begin = item.begin;
+		for (int i = 0; i < item.length; i++) {
+			elements[i] = item.elements[i];
+		}
+	}
+	~Queue() {
+		delete[] elements;
+		length = 0;
+		currentLength = 0;
+	}
+	void operator= (const Queue& right) {
+		delete[] elements;
+		elements = new int[right.length];
+		length = right.length;
+		currentLength = right.currentLength;
+		begin = right.begin;
+		for (int i = 0; i < right.length; i++) {
+			elements[i] = right.elements[i];
+		}
+	}
 	void push(int x);
 	int pop();
 	int min();
