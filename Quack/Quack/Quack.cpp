9f79768bb6d15e78c76a3aa703b19c091b3e5145
IrishkaP <ira.pechenkina.00@mail.ru>
2020-11-30 15:54:26 +0500

    Все задачки кроме 5

---
+++ Quack/Quack/Quack.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <queue>
+#include "edx-io.hpp"
+using namespace std;
+
+
+// число отправляем в очередь
+void numb(string c) {
+	int x = atoi(c);
+
+};
+
+int main()
+{
+	queue <int> och;
+	string c;
+	io >> c;
+	if (c[0] >= '0' && c[0] <= '9')
+		numb(c);
+	// берем из очереди, кладем в регистр
+	if (c[0] == '>')
+	// берем из регистра, кладем в очередь
+	if (c[0] == '<')
+	// берем два элемента из очереди и выполняем операцию
+	if (c[0] == '+')
+	if (c[0] == '-')
+	if (c[0] == '*')
+	if (c[0] == '/')
+	if (c[0] == '%')
+	if (c[0] == 'Z')
+	if (c[0] == 'J')
+	if (c[0] == 'E')
+	if (c[0] == 'G')
+	if (c[0] == 'Q')
+	if (c[0] == 'P')
+	if (c[0] == 'C')
+
+}
+
+
--- задача 7 (19.10.20)/ConsoleApplication2/ConsoleApplication2.cpp
+++ задача 7 (19.10.20)/ConsoleApplication2/ConsoleApplication2.cpp
@@ -1,4 +1,6 @@
 ﻿#include <iostream>
+#include <stack>
+#include <algorithm>
 #include "edx-io.hpp"
 using namespace std;
 
@@ -8,80 +10,23 @@ class Bad_area
 
 class Queue
 {
-	int* elements;
-	int length;
-	int currentLength;
-	int begin;
-	//stack< pair <int,int> > s1, s2;
+	stack< pair <int,int> > s1, s2;
 
 public:
-	Queue() : length(0), currentLength(0), elements(0) {}
-	Queue(const Queue& item) {
-		elements = new int[item.length];
-		length = item.length;
-		currentLength = item.currentLength;
-		begin = item.begin;
-		for (int i = 0; i < item.length; i++) {
-			elements[i] = item.elements[i];
-		}
-	}
-	~Queue() {
-		delete[] elements;
-		length = 0;
-		currentLength = 0;
-	}
-	void operator= (const Queue& right) {
-		delete[] elements;
-		elements = new int[right.length];
-		length = right.length;
-		currentLength = right.currentLength;
-		begin = right.begin;
-		for (int i = 0; i < right.length; i++) {
-			elements[i] = right.elements[i];
-		}
-	}
 	void push(int x);
 	int pop();
-	int min();
+	int mini();
 };
 
 void Queue::push(int x) {
-	if (elements == 0) {
-		elements = new int[1];
-		elements[0] = x;
-		currentLength = 1;
-		length = 1;
-		begin = 0;
-		return;
-	}
-
-	if (currentLength < length) {
-		elements[currentLength] = x;
-		currentLength++;
-		return;
-	}
-
-	if (currentLength == length) {
-		int* help = new int[length + 1];
-		for (int i = 0; i < currentLength; i++)
-			help[i] = elements[i];
-		help[currentLength] = x;
-		delete[] elements;
-		elements = help;
-		length++;
-		currentLength++;
-		return;
-	}
-	/*
-	int minima = s1.empty() ? new_element : min (new_element, s1.top().second);
-    s1.push (make_pair (new_element, minima));
-	*/
+	int minima;
+	if (!s1.empty())
+		minima = min(x, s1.top().second);
+	else
+		minima = x;
+    s1.push (make_pair (x, minima));
 }
 int Queue::pop() {
-	if (currentLength == 0) throw Bad_area();
-	begin++;
-	return elements[begin - 1];
-	/*
 	if (s2.empty())
 	while (!s1.empty()) {
 		int element = s1.top().first;
@@ -89,23 +34,20 @@ int Queue::pop() {
 		int minima = s2.empty() ? element : min (element, s2.top().second);
 		s2.push (make_pair (element, minima));
 	}
-    result = s2.top().first;
+    int result = s2.top().first;
     s2.pop();
-	*/
+	return result;
 }
-int Queue::min() {
-	if (currentLength == 0) throw Bad_area();
-	int min = elements[begin];
-	for (int i = begin + 1; i < currentLength; i++)
-		if (elements[i] < min)
-			min = elements[i];
-	return min;
-	/*
-	if (s1.empty() || s2.empty())
-	current_minimum = s1.empty ? s2.top().second : s1.top().second;
-else
-	current_minimum = min (s1.top().second, s2.top().second);
-	*/
+int Queue::mini() {
+	int current_minimum;
+	if (s1.empty())
+	 current_minimum =  s2.top().second;
+	else
+		if (s2.empty())
+			current_minimum = s1.top().second;
+		else
+			current_minimum = min (s1.top().second, s2.top().second);
+	return current_minimum;
 }
 
 int main()
@@ -126,7 +68,7 @@ int main()
 				result_pop = res.pop();
 			}
 			if (instruction == '?') {
-				result_min = res.min();
+				result_min = res.mini();
 				io << result_min << "\n";
 			}
 		}
